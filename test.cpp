86732a92e58ec2426183c235c9336684ccfdcc20
Jiabin <jiabin@jiabin-VirtualBox.(none)>
2017-08-05 19:13:40 -0400

    Add the first test cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "Hello World" << endl;
+    cout << argv[1] << endl;
+
+    return 0;
+}
+
