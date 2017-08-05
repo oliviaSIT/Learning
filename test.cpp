c25b7b012d6090a73c5f8e11c501e70c76429543
oliviaSIT <olivia.lee8321@gmail.com>
2017-08-05 19:36:09 -0400

    add fruit

--- test.cpp
+++ test.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main(int argc, char* argv[]) {
     cout << "Hello World" << endl;
 
-    if (argc > 1)
-        cout << argv[1] << endl;
+   for (int i = 1; i < argc; i++)
+        cout << argv[i] << endl;
 
     return 0;
 }
