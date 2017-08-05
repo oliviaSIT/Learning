6c4035092d3233ad9d65f4713cb153f200307900
Jiabin <jiabin@jiabin-VirtualBox.(none)>
2017-08-05 19:18:22 -0400

    read the arg length first

--- test.cpp
+++ test.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     cout << "Hello World" << endl;
-    cout << argv[1] << endl;
+
+    if (argc > 1)
+        cout << argv[1] << endl;
 
     return 0;
 }
