ff5423c2cf28a7bdcca6d03af3e479528f9fa32d
oliviaSIT <olivia.lee8321@gmail.com>
2017-08-28 10:58:00 -0400

    1 use class 2 add checking if overflow ps: class over ;

--- c++primer/cha1/q2.cpp
+++ c++primer/cha1/q2.cpp
@@ -1,37 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <climits>
 
 using namespace std;
 
-int main()
-{
-    int x, y, shang = 1, sign = 0;
-    vector<int> v; 
-   
-    cin >> x ;
-    if (x < 0)
-    {
-        sign = 1;
-        x = -1 * x;
-    }
+class solution {
+public :    
+    int reverse (int x) {
+        int r = 0;
+        while (x != 0) {
+            int val = x % 10;
+            int temp = r * 10 + val;
+            if (temp / 10 != r)
+                return 0;
+            r = temp;
+            x /= 10;  
+            }     
+     return r;
+     }
+};
 
-    while (shang != 0)
-    {
-        shang = x / 10;
-        v.push_back(x % 10);
-        x = x / 10;
-    }
+
+int main() {
+    solution ans;
     
-    int m = v.size()-1;
-    y = v[0] * pow(10, m);
-    for (int n = 1; n <= m; ++n)
-    {
-        y = y + v[n] * pow(10 ,m - n);
-    }
-    if (sign == 1)
-       { y = -1 * y;}
-    
-    cout << y << endl;
+    cout << ans.reverse(100) << endl;
+    cout << ans.reverse(-123) << endl;
+    cout << ans.reverse(INT_MAX) << endl; 
+
     return 0;
 }
