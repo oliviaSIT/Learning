ba6b213762f4ed7e36a48b2e3e636d544bbf1690
oliviaSIT <olivia.lee8321@gmail.com>
2017-09-03 18:29:07 -0400

    unfinish

---
+++ c++primer/cha1/merge.cpp
@@ -0,0 +1,15 @@
+class solution {
+public:
+    ListNode Merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode dummy = new Listnode(0);
+        ListNode pt1 = l1;
+        ListNode pt2 = l2;
+
+        
+    }
+};
+
+
