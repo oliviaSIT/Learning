2018d4def20251c9a68bcf248734876d4617bb07
oliviaSIT <olivia.lee8321@gmail.com>
2017-09-03 23:18:57 -0400

    fail

--- c++primer/cha1/merge.cpp
+++ c++primer/cha1/merge.cpp
@@ -1,3 +1,9 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x), next(null) {}
+}
+
 class solution {
 public:
     ListNode Merge(ListNode l1, ListNode l2) {
@@ -7,8 +13,29 @@ public:
         ListNode dummy = new Listnode(0);
         ListNode pt1 = l1;
         ListNode pt2 = l2;
-
+        ListNode current = dummy;
         
+        while (pt1 != null && pt2 != null) {
+            if (pt1.val <= pt2.val) {
+               current.next = pt1;
+               pt1 = pt1.next;
+               }
+            else {
+               current.next = pt2;
+               pt2 = pt2.next;
+                }
+            
+            current = current.next;
+        }
+        
+        if (pt1 == null) {
+           current.next = pt2;
+           }
+        else {
+           current.next = pt1;
+           }
+        
+        return dummy.next;
     }
 };
 
