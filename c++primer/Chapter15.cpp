3d62292c39f2a006046aee10ef46251805db71c2
oliviaSIT <olivia.lee8321@gmail.com>
2017-10-26 22:44:43 -0400

    test of public private protected

---
+++ c++primer/Chapter15.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+class Base {
+public:
+	int x;
+	
+	Base(int x = 0, int y = 0, int z = 0): 
+		x(x), y(y), z(z) { }
+
+protected:
+	int y;
+
+private:
+	int z;
+};
+
+/*
+class PubDerive: public Base {
+public:
+	PubDerive(int x = 0, int y = 0, int z = 0):
+		Base(x, y, z) { }
+
+	int getPub() {return x;}
+
+	int getPro() {return y;}
+
+	int getPri() {return z;}
+
+};
+
+
+class ProDerive: protected Base {
+public:
+        ProDerive(int x = 0, int y = 0, int z = 0):
+                Base(x, y, z) { }
+
+        int getPub() {return x;}
+
+        int getPro() {return y;}
+
+        int getPri() {return z;}
+
+};
+*/
+
+class PriDerive: private Base {
+public:
+        PriDerive(int x = 0, int y = 0, int z = 0):
+                Base(x, y, z) { }
+
+        int getPub() {return x;}
+
+        int getPro() {return y;}
+
+        int getPri() {return z;}
+
+};
+
+
+int main() {
+//	PubDerive d(1 ,2, 3);
+//	ProDerive d(1 ,2, 3);
+	PriDerive d(1, 2, 3);
+
+	int a = d.x;
+	int b = d.y;
+	int c = d.z;	
+
+	cout << "a=" << a << endl;
+        cout << "b=" << b << endl;
+        cout << "c=" << c << endl;
+	
+	return 0;
+}
