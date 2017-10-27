130e646d40ad44605fa763781f60fed050438560
oliviaSIT <olivia.lee8321@gmail.com>
2017-10-26 23:52:33 -0400

    test Base or PubDerive

--- c++primer/Chapter15.cpp
+++ c++primer/Chapter15.cpp
@@ -18,7 +18,7 @@ private:
 	int z;
 };
 
-/*
+
 class PubDerive: public Base {
 public:
 	PubDerive(int x = 0, int y = 0, int z = 0):
@@ -28,11 +28,14 @@ public:
 
 	int getPro() {return y;}
 
-	int getPri() {return z;}
+	int getPri() {
+		cout << "PubDerive" << endl;
+		return Base::getPri();
+	}
 
 };
 
-
+/*
 class ProDerive: protected Base {
 public:
         ProDerive(int x = 0, int y = 0, int z = 0):
@@ -46,7 +49,7 @@ public:
 
 };
 */
-
+/*
 class PriDerive: private Base {
 public:
         PriDerive(int x = 0, int y = 0, int z = 0):
@@ -56,25 +59,27 @@ public:
 
         int getPro() {return y;}
 
-        int Base::getPri() {return z;}
+        int getPri() {return Base::getPri();}
 
 };
-
+*/
 
 int main() {
-//	PubDerive d(1 ,2, 3);
+	Base B = PubDerive(1, 2, 3);
+	cout << B.getPri() << endl;
+	PubDerive d(1 ,2, 3);
 //	ProDerive d(1 ,2, 3);
-	PriDerive d(1, 2, 3);
+//	PriDerive d(1, 2, 3);
 	int ans1 = d.getPub();
-	int ans2 = d.getPro();
+//	int ans2 = d.getPro();
 	int ans3 = d.getPri();
-	int a = d.x;
-	int b = d.y;
-	int c = d.z;	
+//	int a = d.x;
+//	int b = d.y;
+//	int c = d.z;	
 
-	cout << "a=" << a << endl;
-        cout << "b=" << b << endl;
-        cout << "c=" << c << endl;
+//	cout << "a=" << a << endl;
+  //      cout << "b=" << b << endl;
+    //    cout << "c=" << c << endl;
 	
 	return 0;
 }
